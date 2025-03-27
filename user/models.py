1e3ec4ab59098076dfc8ddf277ad21fd88a2422e
Xalid <siyovushartigaliyev12@gmail.com>
2025-03-27 23:17:31 +0500

    Initial commit

---
+++ user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, null=True, blank=True)
+    image = models.ImageField(upload_to='', null=True, blank=True)
+
+    def __str__(self):
+        return "%s %s" % (self.firstname, self.last_name)
